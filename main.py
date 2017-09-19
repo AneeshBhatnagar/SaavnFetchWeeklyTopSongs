b4c9fe43a7d8e140acf559516f3d40b4fc554598
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-09-19 14:58:33 -0400

    Formatted Code and Added Finding Difference between 2 sets of data

--- main.py
+++ main.py
@@ -5,20 +5,78 @@
 '''
 import urllib
 from lxml import html
+import os.path
 
-# url = "https://www.saavn.com/s/featured/hindi/Weekly+Top+Songs"
-#content = urllib.urlopen(url).read()
-f = open('test.txt')
-lines = f.readlines()
-f.close()
-lines = ''.join(lines)
-page = html.fromstring(lines)
-songs = []
-albums = []
+def fetchDataFromWeb(url):
+	return urllib.urlopen(url).read()	
 
-for song in page.xpath("//p[contains(@class, 'song-name')]"):
-	songs.append(song.text)
-for album in page.xpath("//p[contains(@class, 'album-name')]"):
-	albums.append(album.text)
-for i in xrange(len(songs)):
-	print songs[i] + '\t' + albums[i]
+def readHTMLFromFile(filename):
+	f = open(filename)
+	lines = ''.join(f.readlines())
+	f.close()
+	return lines
+
+def findDetailsInHTML(data):
+	page = html.fromstring(data)
+	songs = []
+	albums = []
+	data = []
+
+	for song in page.xpath("//p[contains(@class, 'song-name')]"):
+		songs.append(song.text)
+	for album in page.xpath("//p[contains(@class, 'album-name')]"):
+		albums.append(album.text)
+	for i in xrange(len(songs)):
+		data.append([songs[i],albums[i]])
+	return data
+
+def writeListToFile(data, filename):
+	f = open(filename, 'w')
+	for item in data:
+		f.write("%s\t%s\n" % (item[0], item[1]))
+	f.close()
+
+def readListFromFile(filename):
+	f = open(filename)
+	lines = f.readlines()
+	output = []
+	for line in lines:
+		x = line.strip().split("\t")
+		output.append([x[0],x[1]])
+	f.close()
+	return output
+
+def findDifferenceInData(old,new):
+	oldHashMap = {x[0]+","+x[1]:i+1 for i,x in enumerate(old)}
+	newHashMap = {x[0]+","+x[1]:i+1 for i,x in enumerate(new)}
+	addedTracks = []
+	removedTracks = []
+	movedTracks = []
+	for track in newHashMap.keys():
+		if track not in oldHashMap:
+			temp = track.split(",")
+			addedTracks.append([temp[0],temp[1]])
+		else:
+			if oldHashMap[track] != newHashMap[track]:
+				temp = track.split(",")
+				movedTracks.append([temp[0],temp[1],oldHashMap[track],newHashMap[track]])
+	for track in oldHashMap.keys():
+		if track not in newHashMap:
+			temp = track.split(",")
+			removedTracks.append([temp[0],temp[1]])
+	return addedTracks, removedTracks, movedTracks
+
+
+if __name__ == '__main__':
+	oldData = []
+	oldDataFlag = False
+	# if os.path.isfile('newdata.txt'):
+	# 	oldData = readListFromFile('newdata.txt')
+	# 	writeListToFile(oldData,'oldData.txt')
+	# 	oldDataFlag = True
+	oldData = readListFromFile('oldData.txt')
+	oldDataFlag = True
+	url = "https://www.saavn.com/s/featured/hindi/Weekly+Top+Songs"
+	newData = findDetailsInHTML(readHTMLFromFile('test.txt'))
+	if oldDataFlag:
+		print findDifferenceInData(oldData, newData)
