799d7615d1916a3302d2aabdf29576d8dd757915
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-09-22 00:55:46 -0400

    Added Text Formatting and Colors

--- main.py
+++ main.py
@@ -6,7 +6,15 @@
 import urllib
 from lxml import html
 import os.path
+import datetime as dt
 from tabulate import tabulate
+
+class colors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    ENDC = '\033[0m'
 
 def fetchDataFromWeb(url):
 	return urllib.urlopen(url).read()	
@@ -55,11 +63,16 @@ def findDifferenceInData(old,new):
 	for track in newHashMap.keys():
 		if track not in oldHashMap:
 			temp = track.split(",")
-			newTracks.append([newHashMap[track],temp[0],temp[1],"Newly Added","-"])
+			newTracks.append([newHashMap[track],colors.BLUE + temp[0],temp[1],"Newly Added" + colors.ENDC,"-"])
 		else:
 			temp = track.split(",")
 			if oldHashMap[track] != newHashMap[track]:
-				newTracks.append([newHashMap[track],temp[0],temp[1],oldHashMap[track],newHashMap[track]])
+				newPosition = ''
+				if oldHashMap[track] < newHashMap[track]:
+					newPosition = colors.RED + str(newHashMap[track]) + colors.ENDC
+				else:
+					newPosition = colors.GREEN + str(newHashMap[track]) + colors.ENDC
+				newTracks.append([newHashMap[track],temp[0],temp[1],newPosition,oldHashMap[track]])
 			else:
 				newTracks.append([newHashMap[track],temp[0],temp[1],"-","-"])
 	i = 1
@@ -74,10 +87,10 @@ def displayList(data, removedData = None):
 	if removedData == None:
 		for i in xrange(len(data)):
 			data[i].insert(0,i+1)
-		print tabulate(data, headers = ['Position','Song Title','Album'])
+		print tabulate(data, headers = [colors.YELLOW + 'Position','Song Title', 'Album' + colors.ENDC])
 	else:
 		data = sorted(data, key = lambda d:d[0])
-		print tabulate(data, headers = ['Positon','Song Title', 'Album', 'Current Position', 'Old Position'])
+		print tabulate(data, headers = [colors.YELLOW + 'Positon','Song Title', 'Album', 'Current Position','Old Position' + colors.ENDC])
 		if removedData:
 			print "Some songs were removed. Here is a list of those"
 			print tabulate(removedData, headers = ['S.No','Song Title','Album'])
@@ -86,19 +99,21 @@ def displayList(data, removedData = None):
 if __name__ == '__main__':
 	oldData = []
 	oldDataFlag = False
+	date = dt.datetime.today().strftime("%m/%d/%Y")
 	if os.path.isfile('newData.txt'):
 	 	oldData = readListFromFile('newData.txt')
 	 	oldDataFlag = True
 	url = "https://www.saavn.com/s/featured/hindi/Weekly+Top+Songs"
-	newData = findDetailsInHTML(readHTMLFromFile('test.txt'))
+	#newData = findDetailsInHTML(readHTMLFromFile('test.txt'))
+	newData = findDetailsInHTML(fetchDataFromWeb(url))
 	if oldDataFlag:
 		if oldData == newData:
 			print "The Saavn list is not updated yet. You have the latest playlist already."
-			displayLatestList(newData)
+			displayList(newData)
 		else:
 			print "The Saavn list has been updated. Here is the new list."
-		 	#writeListToFile(oldData,'oldData.txt')
-		 	#writeListToFile(newData, 'newData.txt')
+		 	writeListToFile(oldData,'oldData-%s.txt'%date)
+		 	writeListToFile(newData, 'newData.txt')
 			difference, removed = findDifferenceInData(oldData, newData)
 			displayList(difference, removed)
 	else:
