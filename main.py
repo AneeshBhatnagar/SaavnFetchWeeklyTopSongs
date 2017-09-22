db19afdc4295cb07f00e2597e3552dd8e8a1f58f
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-09-22 00:17:22 -0400

    Created finding difference between new and old list, if it exists

--- main.py
+++ main.py
@@ -50,40 +50,57 @@ def readListFromFile(filename):
 def findDifferenceInData(old,new):
 	oldHashMap = {x[0]+","+x[1]:i+1 for i,x in enumerate(old)}
 	newHashMap = {x[0]+","+x[1]:i+1 for i,x in enumerate(new)}
-	addedTracks = []
 	removedTracks = []
-	movedTracks = []
+	newTracks = []
 	for track in newHashMap.keys():
 		if track not in oldHashMap:
 			temp = track.split(",")
-			addedTracks.append([temp[0],temp[1]])
+			newTracks.append([newHashMap[track],temp[0],temp[1],"Newly Added","-"])
 		else:
+			temp = track.split(",")
 			if oldHashMap[track] != newHashMap[track]:
-				temp = track.split(",")
-				movedTracks.append([temp[0],temp[1],oldHashMap[track],newHashMap[track]])
+				newTracks.append([newHashMap[track],temp[0],temp[1],oldHashMap[track],newHashMap[track]])
+			else:
+				newTracks.append([newHashMap[track],temp[0],temp[1],"-","-"])
+	i = 1
 	for track in oldHashMap.keys():
 		if track not in newHashMap:
 			temp = track.split(",")
-			removedTracks.append([temp[0],temp[1]])
-	return addedTracks, removedTracks, movedTracks
+			removedTracks.append([i, temp[0],temp[1]])
+			i+=1
+	return newTracks, removedTracks
 
-def displayLatestList(data):
-	for i in xrange(len(data)):
-		data[i].insert(0,i+1)
-	print tabulate(data, headers = ['Position','Title','Album'])
+def displayList(data, removedData = None):
+	if removedData == None:
+		for i in xrange(len(data)):
+			data[i].insert(0,i+1)
+		print tabulate(data, headers = ['Position','Song Title','Album'])
+	else:
+		data = sorted(data, key = lambda d:d[0])
+		print tabulate(data, headers = ['Positon','Song Title', 'Album', 'Current Position', 'Old Position'])
+		if removedData:
+			print "Some songs were removed. Here is a list of those"
+			print tabulate(removedData, headers = ['S.No','Song Title','Album'])
 
 
 if __name__ == '__main__':
 	oldData = []
 	oldDataFlag = False
-	# if os.path.isfile('newdata.txt'):
-	# 	oldData = readListFromFile('newdata.txt')
-	# 	writeListToFile(oldData,'oldData.txt')
-	# 	oldDataFlag = True
-	oldData = readListFromFile('oldData.txt')
-	oldDataFlag = True
+	if os.path.isfile('newData.txt'):
+	 	oldData = readListFromFile('newData.txt')
+	 	oldDataFlag = True
 	url = "https://www.saavn.com/s/featured/hindi/Weekly+Top+Songs"
 	newData = findDetailsInHTML(readHTMLFromFile('test.txt'))
-	displayLatestList(newData)
-	#if oldDataFlag:
-		#print findDifferenceInData(oldData, newData)
+	if oldDataFlag:
+		if oldData == newData:
+			print "The Saavn list is not updated yet. You have the latest playlist already."
+			displayLatestList(newData)
+		else:
+			print "The Saavn list has been updated. Here is the new list."
+		 	#writeListToFile(oldData,'oldData.txt')
+		 	#writeListToFile(newData, 'newData.txt')
+			difference, removed = findDifferenceInData(oldData, newData)
+			displayList(difference, removed)
+	else:
+		print "Here is this week's Saavn Top 30 Bollywood songs list."
+		displayList(newData)
